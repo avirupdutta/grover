d1799b041dd0d5359a7114002e3b1c4c26027051
avirupdutta <finalguider99@gmail.com>
2019-06-25 20:39:09 +0530

    added the markup of wide search bar in the navbar

--- views/src/templates/navbar.php
+++ views/src/templates/navbar.php
@@ -1,15 +1,15 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="pl-lg-5 pr-lg-5 navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="./index.php">
     Grover
     <!-- <img class="w-25" src="../images/logo.png" alt="logo"> -->
   </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
@@ -18,15 +18,14 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Offers</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li> 
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-      <button class="btn btn-warning my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
+    <form class="form-inline input-group w-50 text-center mr-auto">
+      <input class="form-control" type="text" placeholder="Search...">
+      <div class="input-group-append">
+        <button class="btn btn-warning" type="submit"><i class="fa fa-search"></i></button>
+      </div>
     </form>
-    <a name="" id="" class="btn btn-secondary mr-sm-1" href="./login.php" role="button">Login</a>
-    <a name="" id="" class="btn btn-success" href="./signup.php" role="button">Sign Up</a>
+    <a name="" id="" class="btn btn-light mr-sm-1" href="./login.php" role="button">Login <i class="fas fa-sign-in-alt"></i></a>
+    <a name="" id="" class="btn btn-success" href="./signup.php" role="button">Sign Up <i class="fas fa-user-plus"></i></a>
   </div>
 </nav>
