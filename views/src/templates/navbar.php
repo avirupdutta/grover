ca716993a54b7827fe99f1f3e93fa3159022d714
avirupdutta <finalguider99@gmail.com>
2019-07-02 11:37:38 +0530

    customised the navbar

--- views/src/templates/navbar.php
+++ views/src/templates/navbar.php
@@ -1,25 +1,26 @@
-<nav class="pl-lg-5 pr-lg-5 navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="./index.php">
-    Grover
-    <!-- <img class="w-25" src="../images/logo.png" alt="logo"> -->
-  </a>
+<nav class="pl-lg-5 pr-lg-5 navbar navbar-expand-lg navbar-dark bg-base">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  
+  <a class="navbar-brand" href="./index.php">
+    <!-- Grover -->
+    <img class="nav-logo w-100" src="../images/logo.png" alt="logo">
+  </a>
 
   <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    <ul class="navbar-nav mr-lg-5">
+      <li class="nav-item ml-3">
+        <a class="nav-link" href="#">Home <i class="fas fa-home"></i> <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Categories</a>
+      <li class="nav-item ml-3">
+        <a class="nav-link" href="#">Categories <i class="fas fa-list-alt"></i></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Offers</a>
+      <li class="nav-item ml-3">
+        <a class="nav-link" href="#">Best Offers <i class="fas fa-tags"></i></a>
       </li>
     </ul>
-    <form class="form-inline input-group w-50 text-center mr-auto">
+    <form class="form-inline input-group text-center mr-auto">
       <input class="form-control" type="text" placeholder="Search...">
       <div class="input-group-append">
         <button class="btn btn-warning" type="submit"><i class="fa fa-search"></i></button>
