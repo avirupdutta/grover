3ba7a7ac2b535735907eaebd36e3094debe4b1bc
anasuyabose2604 <santai61277@yahoo.com>
2019-08-19 00:37:29 +0530

    added div_sign up & h1,Sign up

--- src/views/templates/signup.php
+++ src/views/templates/signup.php
@@ -3,6 +3,15 @@
 
 <!-- CUSTOM STYLE -->
 <link rel="stylesheet" href="<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<style>
+    <body>
+        {
+            margin :0;
+            padding:0;
+        }
+
+
+    </body>
 </head>
 <body>
     <?php require_once("navbar.php"); ?>
@@ -12,6 +21,8 @@
         <div class="row">
             <div class="col-sm-3"></div>
             <div class="col-sm-6">
+            <div class="sign-up-form">
+            <h1> Sign Up Nowv</h1>
                 <form action="../../controllers/process/login.process.php" method="post">
                     <input type="text"class="input-box" placeholder="Enter your First-Name">
                     <input type="text"class="input-box" placeholder="Enter your First-Name"> 
